5898b0e0f4b763367b3d6e22aa95e30ac4e46503
Eugene <eugenemutuyimana15@gmail.com>
2023-10-30 14:26:48 +0200

    added cors

--- api/bm/bm_app.py
+++ api/bm/bm_app.py
@@ -1,11 +1,14 @@
 from api.bm.views import bm_views
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from models import session
 from models import *
 from models.user import User
 
 app = Flask(__name__)
 app.register_blueprint(bm_views)
+cors = CORS(app, resources={r"api/bm/*": {"origins": "*"}})
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
@@ -15,14 +18,11 @@ def teardown_db(exception):
     """
     session.close()
 
+
 @app.route('/', strict_slashes=False)
 def homeroute():
     return "hello"
 
-# @app.route('/users', methods=['GET'])
-# def get_or_post_user():
-#     """this methods is used to GET and POST methods for users"""
-#     return jsonify(get_object())
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
